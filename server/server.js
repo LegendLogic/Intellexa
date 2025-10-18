69b46d8c5c82a742f6930b511d48b5c4a39496a3
Sangram Das <sangramraju143@gmail.com>
2025-10-18 14:17:19 +0530

    Add a new features in a full stack developments

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connetDB  from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouters from './routes/userRoute.js';
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connetDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+//API Routes
+app.use('/api/user', userRouters);
+
+
+app.get('/',(req,res)=>{
+    res.send('API WORKING Great')
+})
+
+app.listen(port, ()=> console.log("Server Started",port))
